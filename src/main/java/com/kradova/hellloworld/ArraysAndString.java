c413382ec001535c16f2285d75b793fd8ad17212
kradova <ksenia.radova@gmail.com>
2022-06-24 00:04:58 +0300

    + Arrays And String

---
+++ src/main/java/com/kradova/hellloworld/ArraysAndString.java
@@ -0,0 +1,22 @@
+package com.kradova.hellloworld;
+
+public class ArraysAndString {
+
+    public static void main(String[] args) {
+       String message = "Hello World";
+       System.out.println(message);
+       
+       String anotherMessage = "";
+       System.out.println(anotherMessage);
+       
+       String myName = "Hello World," + "my name is Jamie";
+       System.out.println(myName);
+       
+    
+    // МЕТОДЫ STRING    "Hello World".length();
+    int myLength = "Hello World".length();
+    System.out.println(myLength);
+    
+    
+    }       
+}
