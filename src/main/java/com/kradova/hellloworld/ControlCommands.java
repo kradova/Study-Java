205c48d7825acad79ce078cae44a85f9e9207566
kradova <ksenia.radova@gmail.com>
2022-07-25 18:50:12 +0300

    + command switch

--- src/main/java/com/kradova/hellloworld/ControlCommands.java
+++ src/main/java/com/kradova/hellloworld/ControlCommands.java
@@ -24,5 +24,23 @@ public class ControlCommands {
         int myNum = 3 > 2 ? 10 : 5;
         System.out.println(myNum);
 
+        System.out.print("Enter your grade: ");
+        String userGrade = input.nextLine().toUpperCase();
+
+        switch (userGrade) {
+            case "A+":
+            case "A":
+                System.out.println("Distinction");
+                break;
+            case "B":
+                System.out.println("B Grade");
+                break;
+            case "C":
+                System.out.println("C Grade");
+                break;
+            default:
+                System.out.println("Fail");
+                break;
+        }
     }
 }
