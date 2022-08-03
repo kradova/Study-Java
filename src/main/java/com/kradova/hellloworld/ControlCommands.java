1116342a006211bb7038977647e9f5dec0a2f867
kradova <ksenia.radova@gmail.com>
2022-08-03 14:47:01 +0300

    + command continue

--- src/main/java/com/kradova/hellloworld/ControlCommands.java
+++ src/main/java/com/kradova/hellloworld/ControlCommands.java
@@ -79,6 +79,13 @@ public class ControlCommands {
                 break;
             }
         }
-
+        //command continue
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+            if (i == 2) {
+                continue;
+            }
+            System.out.println("I will not be printed if i=2.");
+        }
     }
 }
